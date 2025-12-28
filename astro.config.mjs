d6757c0c2499a13e2034be1a75aaf1e0d0b5bfc1
Marce <marce@example.com>
2025-12-28 17:08:11 -0300

    fix: adjust assets path for GitHub Pages sub-directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,12 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: 'https://whitesmoke-snail-585456.hostingersite.com',
-  base: '/',
+  site: process.env.BUILD_DESTINATION === 'hostinger'
+    ? 'https://whitesmoke-snail-585456.hostingersite.com'
+    : 'https://thefolder89.github.io',
+  base: (process.env.GITHUB_ACTIONS && process.env.BUILD_DESTINATION !== 'hostinger')
+    ? '/website-oreidatela/'
+    : '/',
   output: 'static',
   build: {
     format: 'file'
